af9449bf512db9926551e9df25216815a619b29f
3dprintsbyrodrigo <3dprintsbyrodrigo@gmail.com>
2026-02-21 15:25:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Contenido de script.js
+// Scroll suave para enlaces internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
